6a000a1ac4babe10915f7f79346efd8cabfb5fcd
inha <pih5748@likilion.org>
2020-10-25 05:54:33 +0900

    .

--- comma/comma/settings.py
+++ comma/comma/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django-heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,7 +34,7 @@ DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
