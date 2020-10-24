16882c00fc1286d0045c8fff83cc39dfb4c9efb4
dbqdugns24 <dbqudgns24@likelion.org>
2020-10-24 18:53:11 +0900

    first commit

--- comma/comma/settings.py
+++ comma/comma/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!=3+w$-nw3j&yzcedl(o@-%8orl1_1483j7n1$5#n2w(_zvcvr'
+# SECRET_KEY = '!=3+w$-nw3j&yzcedl(o@-%8orl1_1483j7n1$5#n2w(_zvcvr'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
